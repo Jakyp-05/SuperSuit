dcf57137eb1165f95fa3fe1d346e0f8936940b18
Jakyp-05 <jakypjumagulov080gmail.com>
2025-01-23 14:02:19 +0600

    add react-error-boundary

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,10 @@
+import { withErrorBoundary } from "react-error-boundary";
 import MyRoutes from "./routes/routes";
 
 function App() {
   return <MyRoutes />;
 }
 
-export default App;
+export default withErrorBoundary(App, {
+  fallback: <div>Something went wrong!</div>,
+});
