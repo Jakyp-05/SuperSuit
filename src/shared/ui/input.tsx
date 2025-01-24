8137093ebf42f34312d08db10c2a255e0aaad85d
Jakyp-05 <jakypjumagulov080gmail.com>
2025-01-24 17:05:09 +0600

    add input component

--- src/shared/ui/input.tsx
+++ src/shared/ui/input.tsx
@@ -1,5 +1,30 @@
-const Input = () => {
-  return <label>Input</label>;
+import styled from "styled-components";
+
+import React from "react";
+
+type Props = React.DetailedHTMLProps<
+  React.InputHTMLAttributes<HTMLInputElement>,
+  HTMLInputElement
+> & {};
+
+const Input = (props: Props) => {
+  return <InputContainer {...props} />;
 };
 
 export default Input;
+
+const InputContainer = styled.input`
+  width: 100%;
+  padding: 16px;
+  border: 1px solid #e5e5e5;
+  background-color: transparent;
+  color: #041a3f66;
+  font-family: "Montserrat Alternates";
+  font-size: 16px;
+  font-weight: 400;
+  line-height: 25px;
+  margin-top: 16px;
+  &:focus {
+    outline: none;
+  }
+`;
