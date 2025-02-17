e2d597926a0b9ab411c561e5528f6a1e5d7d3955
Jakyp-05 <jakypjumagulov080gmail.com>
2025-02-17 19:03:32 +0600

    super suit

---
+++ src/components/home/home.tsx
@@ -0,0 +1,12 @@
+import { Container } from "../../shared/ui/container";
+import ViewProduct from "../viewProduct";
+
+const Home = () => {
+  return (
+    <Container>
+      <ViewProduct />
+    </Container>
+  );
+};
+
+export default Home;
---
+++ src/components/products/index.tsx
@@ -0,0 +1,37 @@
+import styled from "styled-components";
+
+import { FC } from "react";
+
+import Card from "../card";
+
+const CardContainer = styled.ul`
+  list-style-type: none;
+  display: grid;
+  grid-template-columns: repeat(4, 1fr);
+  grid-gap: 20px;
+  padding: 0 20px;
+  margin: 0;
+
+  @media screen and (min-width: 768px) and (max-width: 1024px) {
+    grid-template-columns: repeat(3, 1fr);
+  }
+  @media screen and (min-width: 640px) and (max-width: 768px) {
+    grid-template-columns: repeat(2, 1fr);
+  }
+  @media only screen and (max-width: 640px) {
+    grid-template-columns: 1fr;
+    place-items: center;
+  }
+`;
+
+const Products:FC = () => {
+  return (
+    <div>
+      <CardContainer>
+        <Card />
+      </CardContainer>
+    </div>
+  );
+};
+
+export default Products;
